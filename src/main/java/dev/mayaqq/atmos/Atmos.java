23378d72e62491ecda921208df3135064cf040cf
mayaqqdev <i@mayaqq.dev>
2023-12-18 21:37:01 +0100

    Added some base stuff like datagen and common dependencies.

--- src/main/java/dev/mayaqq/atmos/Atmos.java
+++ src/main/java/dev/mayaqq/atmos/Atmos.java
@@ -1,13 +1,21 @@
 package dev.mayaqq.atmos;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Atmos implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+
+    public static final String MOD_ID = "atmos";
+    public static final Logger LOGGER = LoggerFactory.getLogger("Atmos");
+
     @Override
     public void onInitialize() {
 
+    }
+
+    public static Identifier id(String path) {
+        return new Identifier(MOD_ID, path);
     }
 }
--- src/main/java/dev/mayaqq/atmos/client/AtmosClient.java
+++ src/main/java/dev/mayaqq/atmos/client/AtmosClient.java
@@ -3,9 +3,7 @@ package dev.mayaqq.atmos.client;
 import net.fabricmc.api.ClientModInitializer;
 
 public class AtmosClient implements ClientModInitializer {
-    /**
-     * Runs the mod initializer on the client environment.
-     */
+
     @Override
     public void onInitializeClient() {
 
