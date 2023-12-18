dc57e5876da655297615e42e8bc6718671fb9520
mayaqqdev <i@mayaqq.dev>
2023-12-18 21:11:33 +0100

    Initial commit

---
+++ src/main/java/dev/mayaqq/atmos/Atmos.java
@@ -0,0 +1,13 @@
+package dev.mayaqq.atmos;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Atmos implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/mayaqq/atmos/client/AtmosClient.java
@@ -0,0 +1,13 @@
+package dev.mayaqq.atmos.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtmosClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
